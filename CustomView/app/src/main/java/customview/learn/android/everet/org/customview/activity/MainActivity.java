0f56eaf20916e3155d4f5b9af7906f59ac8423c4
Cedric Porter <cedricporter@gmail.com>
2014-12-06 19:00:14 +0800

    OptionColorView

---
+++ CustomView/app/src/main/java/customview/learn/android/everet/org/customview/activity/MainActivity.java
@@ -0,0 +1,31 @@
+package customview.learn.android.everet.org.customview.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
+
+import customview.learn.android.everet.org.customview.R;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.activity_main, menu);
+//        return true;
+//    }
+
+    public void onClicked(View view) {
+        String text = view.getId() == R.id.view1 ? "Background" : "Foreground";
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show();
+    }
+
+}
